022dec409cb05ead0e486b309df3b1c8c942e44d
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2017-02-02 18:28:07 +0100

    Added text change event and background color change for the hex input

--- hexhex/MainViewController.swift
+++ hexhex/MainViewController.swift
@@ -13,6 +13,23 @@ class MainViewController: UIViewController {
     @IBOutlet weak var hexInput: UITextField!
     @IBOutlet weak var uicolorInput: UITextField!
     
+    @IBAction func hexInputEditingChanged(_ sender: AnyObject) {
+        print("hexInput: \(hexInput.text)")
+        
+        guard let hexString = hexInput.text else {
+            print("No hex input")
+            return
+        }
+        
+        let color = UIColor(hex: hexString)
+        
+        if hexString.characters.count > 5 {
+            self.view.backgroundColor = color
+        } else {
+            self.view.backgroundColor = UIColor.white
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
