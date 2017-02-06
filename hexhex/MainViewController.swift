eb2f8d57c91ce7d26c788a17c3ed7a7c9147c34e
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2017-02-06 19:52:37 +0100

    Added help functions and comments. Minor adjustments for better reading

--- hexhex/MainViewController.swift
+++ hexhex/MainViewController.swift
@@ -9,7 +9,24 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    //MARK: - Help Methods for resetting values and background
+    func resetHexInputAndBackground() {
+        hexInput.text = ""
+        self.view.backgroundColor = UIColor.white
+    }
+    
+    func resetUIColorInputsAndBackground() {
+        redInput.text = ""
+        greenInput.text = ""
+        blueInput.text = ""
+        alphaInput.text = "1.00"
+        self.view.backgroundColor = UIColor.white
 
+    }
+
+    
+    
     //MARK: - Outlets & Actions
     
     @IBOutlet weak var hexInput: UITextField!
@@ -19,70 +36,75 @@ class MainViewController: UIViewController {
     @IBOutlet weak var blueInput: UITextField!
     @IBOutlet weak var alphaInput: UITextField!
     
+    //MARK: - Button action. Convert from HEX to UIColor
     @IBAction func hexToUIColorAction(_ sender: Any) {
         
         guard let hexString = hexInput.text else {
             print("no hex input")
-            self.view.backgroundColor = UIColor.white
+            resetUIColorInputsAndBackground()
             return
         }
         
         guard let color = UIColor(hex: hexString) else {
             print("cannot build the uicolor with this string: \(hexString)")
-            self.view.backgroundColor = UIColor.white
-            //reset values
-            redInput.text = ""
-            greenInput.text = ""
-            blueInput.text = ""
-            alphaInput.text = "1.00"
+            ////reset values and window color
+            resetUIColorInputsAndBackground()
             return
         }
         
+        //at this point UIColor Object was created successfully
+        //set background color to the color
         self.view.backgroundColor = color
         
-        guard let colors = uiColorToString(withUIColor: color) else {
+        //create an String Array with values for the input fields
+        guard let colorStringArray = uiColorToString(withUIColor: color) else {
             print("cannot create colors")
             return
         }
         
-        redInput.text = colors[0]
-        greenInput.text = colors[1]
-        blueInput.text = colors[2]
+        redInput.text = colorStringArray[0]
+        greenInput.text = colorStringArray[1]
+        blueInput.text = colorStringArray[2]
         
-        if colors.count > 3 {
-            alphaInput.text = colors[3]
+        if colorStringArray.count > 3 {
+            alphaInput.text = colorStringArray[3]
         }
     }
     
     
     
-    
+    //MARK: - Button action. Convert from UIColor to HEX
     @IBAction func uiColorToHexAction(_ sender: Any) {
         let fields = [redInput, greenInput, blueInput, alphaInput]
-        var colors: [String] = []
+        var colorStringArray: [String] = []
         
+        //iterate through fields and append elements to array with color strings
         for field in fields {
             guard let color = field?.text else {
-                print("input field empty?")
+                print("cannot read input field")
+                resetHexInputAndBackground()
                 return
             }
-            colors.append(color)
+            colorStringArray.append(color)
         }
         
-        guard let color = stringArrayToUIColor(withColorStringArray: colors) else{
+        guard let color = stringArrayToUIColor(withColorStringArray: colorStringArray) else{
             print("no ui color")
             //reset background and value of hexInput
-            self.view.backgroundColor = UIColor.white
-            hexInput.text = ""
+            resetHexInputAndBackground()
             return
         }
         
+        //at this point a uicolor object was created
+        //fill background with the color
         self.view.backgroundColor = color
         
-        
-        if colors[3] == "1.0" {
+        //check the alpha value and call method with or without the parameter
+        if colorStringArray[3] == "1.0" {
             guard let hexString = color.hexString() else {
                 print("cannot create hex string")
+                //reset background and value of hexInput
+                resetHexInputAndBackground()
                 return
             }
             hexInput.text = hexString
@@ -90,6 +112,8 @@ class MainViewController: UIViewController {
         }else{
             guard let hexString = color.hexString(alpha: true) else {
                 print("cannot create hex string")
+                //reset background and value of hexInput
+                resetHexInputAndBackground()
                 return
             }
             hexInput.text = hexString
